54cb191016d79d7896032edb806d833a0146e6e8
Andrew Carter <andrewcarter1992@gmail.com>
2020-08-18 22:41:50 +0100

    Upversion 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='rockblock-tools',
-    version='0.0.3',
+    version='0.0.4',
 
     description='RockBLOCK message sending and receiving tools',
     long_description=long_description,
